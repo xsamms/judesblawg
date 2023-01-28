4c02e021c9dd98c9c7a3ecce5ac415d99847efcc
Samuel Williams <xsamms@outlook.com>
2023-01-28 17:17:01 +0100

    Postgres details

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("PGHOST", "127.0.0.1"),
-      port: env.int("PGPORT", 5931),
+      host: env("PGHOST", "containers-us-west-150.railway.app"),
+      port: env.int("PGPORT", 5956),
       database: env("PGDATABASE", "railway"),
       user: env("PGUSER", "postgres"),
-      password: env("PGPASSWORD", "password"),
+      password: env("PGPASSWORD", "QON9QGq4SDkInJi6lwll"),
       ssl: env.bool(true),
     },
   },
